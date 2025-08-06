44e9372892a7a7fe4019f54f76e10ee846b90d7c
Jeremy Lincoln <bossco28@gmail.com>
2025-08-06 16:13:08 -0400

    User register

---
+++ resources/js/Components/ErrorMessage.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+    errors: Object
+})
+</script>
+
+<template>
+    <div class="mb-2" v-if="Object.keys(errors).length">
+        <p class="text-sm text-red-500">
+           Something went wrong 
+        </p>
+        <ul class="ml-4 list-disc list-inside">
+            <li 
+              v-for="(error, key) in errors" :key="key" 
+              class="text-sm text-red-500">{{ error }}</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,9 +1,25 @@
 <script setup>
-    import Container from '../../Components/Container.vue';
-    import Title from '../../Components/Title.vue';
-    import TextLink from '../../Components/TextLink.vue';
-    import InputField from '../../Components/InputField.vue';
-    import PrimaryBtn from '../../Components/PrimaryBtn.vue';
+import Container from '../../Components/Container.vue';
+import Title from '../../Components/Title.vue';
+import TextLink from '../../Components/TextLink.vue';
+import InputField from '../../Components/InputField.vue';
+import PrimaryBtn from '../../Components/PrimaryBtn.vue';
+import ErrorMessage from '../../Components/ErrorMessage.vue';
+import { useForm } from "@inertiajs/vue3";
+
+
+const form = useForm({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+});
+
+const submit = () => {
+    form.post(route('register'), {
+        onFinish: () => form.reset('password', 'password_confirmation'),
+    });
+}
 </script>
 
 <template>
@@ -15,16 +31,18 @@
             </p>
         </div>
 
-        <form class="space-y-6">
-            <InputField label="Name" icon="id-badge" type="text" />
-            <InputField label="Email" icon="at" type="email" />
-            <InputField label="Password" icon="key" type="password" />
-            <InputField label="Password Confirmation" icon="key" type="password" />
+        <!-- errors messages -->
+        <ErrorMessage :errors="form.errors"/>
+        <form @submit.prevent="submit" class="space-y-6">
+            <InputField label="Name" icon="id-badge" type="text" v-model="form.name"/>
+            <InputField label="Email" icon="at" type="email" v-model="form.email"/>
+            <InputField label="Password" icon="key" type="password" v-model="form.password"/>
+            <InputField label="Password Confirmation" icon="key" type="password" v-model="form.password_confirmation"/>
 
             <p class="text-slate-500 text-sm dark:text-slate-400">
                 By registering, you agree to the <TextLink routeName="home" label="Terms of Service" /> and <TextLink routeName="home" label="Privacy Policy" />
             </p>
-            <PrimaryBtn disabled>Register</PrimaryBtn>
+            <PrimaryBtn :disabled="form.processing">Register</PrimaryBtn>
         </form>
     </Container>
 </template>
